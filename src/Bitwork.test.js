5648f6ddedc87a1eb761ea3f59e786d75ba051b6
rah <richard.hein@cognilore.com>
2024-03-13 18:28:35 -0400

    Add RxJS and Babel plugin to package.json and update Bitwork.test.js

--- src/Bitwork.test.js
+++ src/Bitwork.test.js
@@ -1,26 +1,24 @@
+import { TestScheduler } from 'rxjs/testing';
+import { throttleTime } from 'rxjs';
 const bitwork = require('bitwork')
 const bit = new bitwork({ rpc: { protocol: "https", host: "miner.entangleit.com", user: "root", pass: "nfhRO+3[" }, peer: { host: "miner.entangleit.com", port: 80  } } )
+const testScheduler = new TestScheduler((actual, expected) => {
+  // asserting the two objects are equal - required
+  // for TestScheduler assertions to work via your test framework
+  // e.g. using chai.
+  expect(actual).toStrictEqual(expected);
+});
 
-describe('bitwork operations', () => {
-  test('bitwork ready', async () => {
-    expect(bit).toBeDefined()
-  })  
-  test('bitwork error', async () => {
-    bit.on("error", async (err) => {
-      console.log('bitwork error', err)
-    })
-    expect(bit).toBeDefined()
-  })
-  test('bitwork block', async () => {
-    bit.on("block", async (block) => {
-      console.log('bitwork block', block)
-    })
-    expect(bit).toBeDefined()
-  })
-  test('bitwork tx', async () => {
-    bit.on("tx", async (tx) => {
-      console.log('bitwork tx', tx)
-    })
-    expect(bit).toBeDefined()
-  })
+// This test runs synchronously.
+it('generates the stream correctly', () => {
+  testScheduler.run((helpers) => {
+    const { cold, time, expectObservable, expectSubscriptions } = helpers;
+    const e1 = cold(' -a--b--c---|');
+    const e1subs = '  ^----------!';
+    const t = time('   ---|       '); // t = 3
+    const expected = '-a-----c---|';
+
+    expectObservable(e1.pipe(throttleTime(t))).toBe(expected);
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
+  });
 });
